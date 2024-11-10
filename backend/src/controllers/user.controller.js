9cd9c1876f8d1d868e4af53ddfd271932abe8e19
Harshkr007 <harshkr1890@gmail.com>
2024-11-11 00:55:19 +0530

    improve admin registration

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,28 +1,34 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-const handleUserRegistration = async (req,res) => {
+const handleUserRegistration = async (req, res) => {
     try {
         const { username, email, password } = req.body;
-        const newUer = await User.create({
+        
+        console.log(`${username}, ${email}, ${password}`);
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        const newUser = await User.create({
             username,
             email,
             password,
         });
-        if(!newUer){
-            return res.status(400).json({
-                message: "User registration failed",
-            });
-        }
+
         return res.status(201).json({
             message: "User registered successfully",
-            user: newUer,
+            user: newUser,
         });
     } catch (error) {
-        console.error(error);
+        return res.status(500).json({
+            message: "User registration failed",
+            error: error.message
+        });
     }
 }
-
 const handleUserAdmin = async (req, res) => {
     const {username, password} = req.body;
     try {
@@ -45,15 +51,14 @@ const handleUserAdmin = async (req, res) => {
             });
         }
 
-        const token = jwt.sign(
-            {
-                id: admin._id,
-                username: admin.username,
-                role: admin.role
-            },
-            process.env.JWT_SECRET_KEY,
-            { expiresIn: "1d" }
-        );
+        const token = jwt.sign({
+            id: admin._id,
+            username: admin.username,
+            role: admin.role,
+        }, process.env.JWT_SECRET_KEY, 
+        {
+            expiresIn: "1d",
+        })
 
         return res.status(200).json({
             message: "Authentication successful",
